6d95899216dfab43985bfcaabdeba78f0f05b707
Abbasjuzer14 <93586845+Abbasjuzer14@users.noreply.github.com>
2024-09-18 06:34:29 +0000

    covertiion topic

---
+++ basic_01/ConvertionOperations.js
@@ -0,0 +1,16 @@
+let IsNumber = 33
+console.log(IsNumber);
+console.log(typeof (IsNumber));
+let IsString = String(IsNumber)
+console.log(IsString);
+console.log(typeof (IsString));
+let IsBoolean = Boolean(IsNumber)
+console.log(IsBoolean);
+console.log(typeof (IsBoolean));
+// let IsUndefine = undefined(IsNumber)
+// console.log(IsUndefine);
+// console.log(typeof (IsUndefine));
+// let IsNull = Null(IsNumber)
+// console.log(IsNull);
+// console.log(typeof (IsNull));
+
