c283bca24262483509127b640aa1337bacb6a5e9
Abbasjuzer14 <93586845+Abbasjuzer14@users.noreply.github.com>
2024-09-18 05:54:18 +0000

    changes in variable and constant

---
+++ basic_01/VariableInfo.js
@@ -0,0 +1,7 @@
+const studentId = 2635
+var email = "abbasjuzer@gmail.com"
+let address = "near sun temple"
+Abbas = "juzer"
+
+// console.log(studentId);
+console.table([studentId,email,address,Abbas])
